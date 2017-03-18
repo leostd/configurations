f11323538c54aaffd9f6a1f10fa3c6c3626e401a
Leonardo Santella <leocaracas2010@gmail.com>
2017-03-17 23:35:46 -0400

    added new default programs

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,5 +1,5 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-
+sudo add-apt-repository ppa:caffeine-developers/ppa
 sudo add-apt-repository ppa:noobslab/macbuntu
 sudo add-apt-repository ppa:videolan/master-daily
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -15,9 +15,11 @@ sudo apt-get install gnome-tweak-tool -y
 sudo apt-get install spotify-client
 sudo apt install vlc -y
 sudo apt-get install git -y
-sudo apt-get install build-essential libssl-dev 
+sudo apt-get install build-essential libssl-dev -y
+sudo apt-get install curl -y
+sudo apt-get install caffeine -y
 
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
 source ~/.profile
-nvm install 6.0.0
+nvm install 6.10.0
